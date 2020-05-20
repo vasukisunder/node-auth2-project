076f4088348e983461bf172c4888db74f423f01f
Vasuki Sunder <vasukisunder@Vasukis-MBP.attlocal.net>
2020-05-19 20:39:27 -0700

    basic structure setup

---
+++ api/router.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const db = require('./db');
+const jwt = require('jsonwebtoken');
+
+router.post('/register', (req, res) => {
+
+})
+
+router.post('/login', (req, res) => {
+    
+})
+
+router.get('/users', (req, res) => {
+    
+})
+
+module.exports = router;
